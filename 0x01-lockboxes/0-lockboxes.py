46faaeda0ca00deb4bb848a7b8486447955d6a9d
nickfff-dev <hunchographics@gmail.com>
2024-03-18 01:15:21 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,36 +4,18 @@
 
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes can be opened.
-
-    Parameters:
-    boxes (list of lists): A list of lists
-    where each list represents the keys in a box.
-
-    Returns:
-    bool: True if all boxes can be opened, else False.
+    function that Determines if all the boxes can be opened.
     """
-    # Create a set of unlocked boxes
-    unlocked_boxes = set()
-    # Start with the first box unlocked
-    unlocked_boxes.add(0)
-
-    def dfs(box):
-        """
-        Depth-first search to unlock all boxes.
-
-        Parameters:
-        box (int): The current box to unlock.
-
-        Returns:
-        bool: True if all boxes can be unlocked, else False.
-        """
-        for key in boxes[box]:
-            if key not in unlocked_boxes:
-                unlocked_boxes.add(key)
-                if not dfs(key):
-                    return False
-        return True
-
-    # Start DFS from the first box
-    return dfs(0)
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for box in range(1, len(boxes) - 1):
+        checked_boxes = False
+        for idx in range(len(boxes)):
+            checked_boxes = box in boxes[idx] and box != idx
+            if checked_boxes:
+                break
+        if checked_boxes is False:
+            return checked_boxes
+    return True
