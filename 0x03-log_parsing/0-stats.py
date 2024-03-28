5600325cb1ed778fbd996336baa25b0bd3e160d4
nickfff-dev <hunchographics@gmail.com>
2024-03-29 01:11:03 +0300

    after 10 lines

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,56 +2,47 @@
 """ Log parsing """
 import sys
 import re
-import signal
-
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                      404: 0, 405: 0, 500: 0}
 
 
-# Function to handle keyboard interruption
-def signal_handler(sig, frame):
-    """ Signal handler """
-    print_statistics()
-    sys.exit(0)
+if __name__ == "__main__":
+    # Function to print statistics
+    def print_statistics(log_data: dict) -> None:
+        """ Print statistics """
+        print("File size: {}".format(log_data['total_file_size']))
+        for status_code in sorted(log_data['status_code_dict']):
+            if log_data['status_code_dict'][status_code]:
+                code = log_data['status_code_dict'][status_code]
+                print("{}: {}".format(status_code, code))
+    num_lines = 0
+    log_data = {'total_file_size': 0, 'status_code_dict':
+                {str(x): 0 for x in [
+                    200, 301, 400, 401, 403, 404, 405, 500
+                ]}
+                }
 
+    # Process each line from stdin
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            # Regular expression to match the input format
+            pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'  # nopep8
+            match = re.match(pattern, line)
 
-# Function to print statistics
-def print_statistics():
-    """ Print statistics """
-    print("File size: {}".format(total_file_size))
-    for status_code in sorted(status_code_counts.keys()):
-        if status_code_counts[status_code] > 0:
-            print("{}: {}".format(status_code,
-                                  status_code_counts[status_code]))
+            if match:
+                num_lines += 1
+                # Extract file size and status code
+                file_size = int(match.group(2))
+                status_code = int(match.group(1))
 
-
-# Register signal handler for keyboard interruption
-signal.signal(signal.SIGINT, signal_handler)
-
-# Process each line from stdin
-for line in sys.stdin:
-    # Regular expression to match the input format
-    pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'  # nopep8
-    match = re.match(pattern, line)
-
-    if match:
-        # Extract file size and status code
-        file_size = int(match.group(3))
-        status_code = int(match.group(2))
-
-        # Update metrics
-        total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-
-    # Print statistics every 10 lines
-    if (total_file_size // 10) * 10 < total_file_size:
-        print_statistics()
-        total_file_size = 0
-        for status_code in status_code_counts.keys():
-            status_code_counts[status_code] = 0
-
-# Print final statistics if any
-if total_file_size > 0:
-    print_statistics()
+                # Update metrics
+                log_data['total_file_size'] += file_size
+                if isinstance(status_code, int) and \
+                        (str(status_code) in log_data['status_code_dict']):
+                    log_data['status_code_dict'][str(status_code)] += 1
+            # Print statistics every 10 lines
+                if num_lines % 10 == 0:
+                    print_statistics(log_data)
+        print_statistics(log_data)
+    except KeyboardInterrupt:
+        print_statistics(log_data)
+        raise
