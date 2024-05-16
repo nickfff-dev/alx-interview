73a3e38e6fa9bf870c446dd474bf3ee1b9e4d44d
nickfff-dev <hunchographics@gmail.com>
2024-05-17 01:54:40 +0300

    remaining

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,7 +9,6 @@ def isWinner(x, nums):
     if x < 1 or not nums:
         return None
     marias_wins, bens_wins = 0, 0
-    # generate primes with a limit of the maximum number in nums
     n = max(nums)
     primes = [True for _ in range(1, n + 1, 1)]
     primes[0] = False
@@ -18,7 +17,6 @@ def isWinner(x, nums):
             continue
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-    # filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
